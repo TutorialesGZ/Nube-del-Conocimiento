9f94ec46f363ec5979daacd03f10c4f847955ac6
David <davidcjaguilarmail.com>
2022-11-24 20:30:19 -0600

    se agrega la primera screen de la actividad 3

--- NubeDelConocimientoAppAlumnot/src/screens/Act3/Sujeto1.tsx
+++ NubeDelConocimientoAppAlumnot/src/screens/Act3/Sujeto1.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableNativeFeedback } from 'react-native'
 
 export const Sujeto1 = () => {
   return (
@@ -15,70 +15,167 @@ export const Sujeto1 = () => {
             borderRadius: 180,
             top: -250,
             left: 50,
+
         }}>
             <Text style={{
                 color: '#FFF',
                 bottom: '-75%',
-                left: '24%',
+                left: '40%',
                 fontWeight: 'bold',
                 fontSize: 18
-            }}>Signos de puntuación</Text>
+            }}>Sujeto</Text>
         </View>
       <Image
         style={{
           position: 'absolute',
-          top: '35%',
-          right: '-20%',
-          height: '15%',
-          width: '30%',
-          transform: [{rotate: '90deg'}],
-        }}
-        //source={require('.../NubeDelConocimientoAppAlumnot/src/img/1.png')}
-      />
-      <Image
-        style={{
-          position: 'absolute',
-          bottom: '15%',
-          left: '-15%',
-          height: '15%',
-          width: '30%',
-        }}
-       // source={require('.../src/img/circulo.png')}
-      />
-      <Image
-        style={{
-          position: 'absolute',
-          bottom: '15%',
+          top: '30%',
           right: '-15%',
           height: '15%',
           width: '30%',
+       
         }}
-        //source={require('.../src/img/circulo.png')}
+        source={require('../Act3/estrella.png')}
+      />
+      <Image
+        style={{
+          position: 'absolute',
+          bottom: '15%',
+          left: '-20%',
+          height: '15%',
+          width: '30%',
+        }}
+        source={require('../Act3/circulo.png')}
+      />
+      <Image
+        style={{
+          position: 'absolute',
+          bottom: '15%',
+          right: '-18%',
+          height: '15%',
+          width: '30%',
+          transform: [{rotate: '180deg'}],
+        }}
+        source={require('../Act3/circulo.png')}
       />
       <Image
         style={{
           position: 'absolute',
           top: '45%',
-          left: '-20%',
+          left: '-15%',
           height: '15%',
           width: '30%',
-          transform: [{rotate: '180deg'}],
         }}
-        //source={require('.../src/img/circulo.png')}
+        source={require('../Act3/triangulo.png')}
       />
+
+      <Text style={{
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginTop: '40%',
+        color: '#2DDA93',
+        paddingHorizontal: '18%',
+      }}>
+        "La casa estaba terminada"
+      </Text>
+      
         <Text style={{
-            borderRadius: 100,
-            top: '20%',
-            fontSize: 14,
+            borderRadius: 20,
+            top: '10%',
+            fontSize: 25,
             color: '#FFF',
             left: '1%',
             width: '90%',
+            fontWeight: 'bold',
             backgroundColor: '#2DDA93',
-            height: '5%',
+            height: '15%',
             paddingVertical: 8,
-            paddingHorizontal: 20,
+            paddingHorizontal: 50,
             marginHorizontal: 20,
-        }}>Arrastra y suelta cada palabra junto a su definición</Text>
+        }}>¿Cuál es el sujeto de la oración mostrada?</Text>
+
+        <View style={{
+            flexDirection: 'row',
+            height: '10%',
+            top: '30%',
+        }}>
+        <TouchableNativeFeedback
+        onPress={() => console.log('Opcion1')}
+        >
+          <View
+            style={{
+              backgroundColor: '#D2ECFE',
+              width: '25%',
+              height: '50%',
+              borderRadius: 40,
+              paddingVertical: 5,
+              left: '10%',
+              bottom: '-5%',
+              elevation: 8,
+              marginLeft: 20,
+              marginRight: 160
+            }}>
+            <Text
+              style={{
+                marginLeft: '27%',
+                marginTop: '2%',
+                color: '#000',
+              }}>
+              La casa
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback
+        onPress={() => console.log('Opcion1')}
+        >
+          <View
+            style={{
+              backgroundColor: 'rgba(255,199,1,100)',
+              width: '25%',
+              height: '50%',
+              borderRadius: 40,
+              paddingVertical: 5,
+              left: '10%',
+              bottom: '-5%',
+              elevation: 8,
+            }}>
+            <Text
+              style={{
+                marginLeft: '27%',
+                marginTop: '2%',
+                color: '#000',
+              }}>
+              estaba
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
+        </View>
+        <TouchableNativeFeedback
+        onPress={() => console.log('Opcion1')}
+        >
+          <View
+            style={{
+              backgroundColor: '#D8D67E',
+              width: '25%',
+              height: '5%',
+              borderRadius: 40,
+              paddingVertical: 5,
+              left: '35%',
+              bottom: '-25%',
+              elevation: 8,
+            }}>
+            <Text
+              style={{
+                marginLeft: '19%',
+                marginTop: '2%',
+                color: '#000',
+              }}>
+              terminada
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
+        <Image 
+        //source={require('../Act3/Robot_sujeto.png')}
+      />
         </View>
   )
 }
